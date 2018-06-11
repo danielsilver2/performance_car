4efa52898ff1b6596c72e4ac2281b5a41eca67fa
danielsilver2 <silver.daniel97@gmail.com>
2018-06-11 20:32:14 -0300

    finished project

--- src/main/java/br/univille/projetoperformance/controller/PacienteController.java
+++ src/main/java/br/univille/projetoperformance/controller/PacienteController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -47,6 +48,17 @@ public class PacienteController {
         
         paciente = this.pacienteRepository.save(paciente);
         
+        return new ModelAndView("redirect:/paciente");
+    }
+    
+    @GetMapping(value="alterar/{id}")
+    public ModelAndView alterarForm(@PathVariable("id") Paciente paciente) {
+        return new ModelAndView("paciente/form","paciente",paciente);
+    }
+    
+    @GetMapping(value="remover/{id}")
+    public ModelAndView remover(@PathVariable ("id") Long id, RedirectAttributes redirect) {
+        this.pacienteRepository.deleteById(id);
         return new ModelAndView("redirect:/paciente");
     }
 }
